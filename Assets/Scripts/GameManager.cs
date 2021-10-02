a551c5e8e9b2acbdcdf78df43ac5f51cee97abdb
DESKTOP-HH77JBR\Antonio <josan.agzb@gmail.com>
2021-10-02 18:21:52 -0500

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private int minutes;
+    private float timeValue;
+    private float lastTimeToHead;
+
+    [SerializeField]
+    private Transform Head;
+    [SerializeField]
+    private int headTimer;
+
+    [SerializeField]
+    private Text timeText;
+
+    [SerializeField]
+    private AudioSource dollSing;
+    [SerializeField]
+    private AudioSource dollHeadOff;
+    [SerializeField]
+    private AudioSource dollHeadOn;
+
+    public static bool headTime;
+    public static bool headTimeFinish;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        headTime = false;
+        timeValue = minutes * 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDown();
+    }
+
+    private void CountDown()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+        }
+
+        DisplayTime(timeValue);
+    }
+
+    private void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0)
+            timeToDisplay = 0;
+
+        float mins = Mathf.FloorToInt(timeToDisplay / 60);
+        float secs = Mathf.FloorToInt(timeToDisplay % 60);
+        HeadTime(secs);
+
+        timeText.text = string.Format("{0:00}:{1:00}", mins, secs);
+    }
+
+    private void HeadTime(float secs)
+    {
+        if (secs <= 0)
+        {
+            headTimeFinish = true;
+            RotHead(180);
+            return;
+        }
+
+        if (secs % headTimer == 0 && secs != lastTimeToHead)
+        {
+            lastTimeToHead = secs;
+            headTime = !headTime;
+
+            if (headTime)
+            {
+                dollHeadOn.Play(0);
+            }
+            else
+            {
+                if (!dollSing.isPlaying)
+                    dollHeadOff.Play(0);
+
+                if (!dollSing.isPlaying)
+                    dollSing.PlayDelayed(1);
+            }
+        }
+
+        if (headTime)
+            RotHead(180);
+        else
+            RotHead(0);
+    }
+
+    private void RotHead(int deg)
+    {
+        Vector3 direction = new Vector3(Head.rotation.eulerAngles.x, deg, Head.rotation.eulerAngles.z);
+        Quaternion targetRotation = Quaternion.Euler(direction);
+        Head.rotation = Quaternion.Lerp(Head.rotation, targetRotation, Time.deltaTime * 3);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 PlayerMovementInput;
+    private Vector2 PlayerMouseInput;
+    private float xRot;
+
+    [SerializeField]
+    private Animator anim;
+
+    private bool isJumping;
+    private bool isWalking;
+    private bool isMoving;
+    private bool isDying;
+    private bool isInDeathZone;
+
+    [SerializeField]
+    private LayerMask FloorMask;
+
+    [SerializeField]
+    private Transform FeetTransform;
+
+    [SerializeField]
+    private Transform PlayerCamera;
+
+    [SerializeField]
+    private Rigidbody PlayerBody;
+
+    [Space]
+
+    [SerializeField]
+    private float Speed;
+
+    [SerializeField]
+    private float Sensitivity;
+
+    [SerializeField]
+    private float Jumpforce;
+
+    [SerializeField]
+    AudioSource feetSteps;
+    [SerializeField]
+    AudioSource shoot;
+
+    [SerializeField]
+    private Transform deathZone;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDying)
+            return;
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        PlayerMovementInput = new Vector3(h, 0f, v);
+        PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        CheckJumping();
+        CheckMoving();
+        MovePlayer();
+        MovePlayerCamera();
+
+        isWalking = (h != 0 || v != 0);
+        anim.SetBool("isWalking", isWalking);
+
+        if (isWalking && !isJumping && !feetSteps.isPlaying)
+        {
+            feetSteps.loop = true;
+            feetSteps.Play(0);
+        }
+        else
+            feetSteps.loop = false;
+
+        CheckDeathTime();
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput) * Speed;
+        PlayerBody.velocity = new Vector3(MoveVector.x, PlayerBody.velocity.y, MoveVector.z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isJumping)
+                PlayerBody.AddForce(Vector3.up * Jumpforce, ForceMode.Impulse);
+        }
+    }
+
+    private void MovePlayerCamera()
+    {
+        xRot -= PlayerMouseInput.y * Sensitivity;
+
+        transform.Rotate(0f, PlayerMouseInput.x * Sensitivity, 0f);
+        PlayerCamera.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+    }
+
+    private void CheckJumping()
+    {
+        isJumping = !Physics.CheckSphere(FeetTransform.position, 0.1f, FloorMask);
+        anim.SetBool("isJumping", isJumping);
+    }
+
+    private void CheckMoving()
+    {
+        isMoving = isJumping || isWalking;
+    }
+
+    private void CheckDeathTime()
+    {
+        if (GameManager.headTime && isMoving || GameManager.headTimeFinish)
+        {
+            if (!isInDeathZone)
+                return;
+
+            isDying = true;
+            anim.SetBool("isDying", true);
+            feetSteps.Stop();
+            shoot.Play(0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isInDeathZone = other.transform == deathZone;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == deathZone)
+            isInDeathZone = false;
+    }
+}
